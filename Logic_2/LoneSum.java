c9ef4a75a1354d583fa492b26033b13501b61dec
Grigory Tenitsky <uram17@mail.ru>
2022-03-26 00:06:30 +0300

    add Logic_2

---
+++ Logic_2/LoneSum.java
@@ -0,0 +1,11 @@
+package Logic_2;
+
+public class LoneSum {
+    public int loneSum(int a, int b, int c) {
+        if(a == b && a == c) return 0;
+        if(a == b) return c;
+        if(b == c) return a;
+        if(a == c) return b;
+        return a + b + c;
+    }
+}
---
+++ Logic_2/MakeBricks.java
@@ -0,0 +1,9 @@
+package Logic_2;
+
+public class MakeBricks {
+    public boolean makeBricks(int small, int big, int goal) {
+        if( goal > big * 5 + small) return false;
+        if(goal % 5 > small) return false;
+        return true;
+    }
+}
